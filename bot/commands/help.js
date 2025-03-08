8b720d2621ecf8247bc7cfde908d46552de75e4a
Administrator <dankoshevoy2001@gmail.com>
2025-03-08 11:30:12 +0300

    init

--- bot/commands/help.js
+++
@@ -1,7 +0,0 @@
-import {getText} from '../utils/helpers.js'
-
-function help(ctx) {
-	return getText('help', ctx.from.language_code)
-}
-
-export default help
--- bot/commands/start.js
+++
@@ -1,6 +0,0 @@
-function start(ctx) {
-	// return	getText("start",ctx.from.language_code)
-	ctx.scene.enter('startScene')
-}
-
-export default start
