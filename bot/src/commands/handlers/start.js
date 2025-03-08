cb689847968019bd58d2ecf626a6170b712a4d98
Administrator <dankoshevoy2001@gmail.com>
2025-03-08 20:11:17 +0300

    fix

--- bot/src/commands/handlers/start.js
+++ bot/src/commands/handlers/start.js
@@ -1,8 +1,17 @@
-const axios = require('axios')
-
-function start(ctx) {
-	// проверка есть ли пользователь в бд и есть ли у него подписка
-	
+async function start(ctx) {
+	fetch('http://host.docker.internal:80/api/checkUser', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			telegram_id: '12331241421'
+		})
+	}).then(res => {
+		if (res.status === 200) {
+			console.log(JSON.stringify(res.data))
+		}
+	})
 }
 
 module.exports = start
