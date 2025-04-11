95cf7732dc094f3bb6ebbc1c74195dd5b19fbf29
Administrator <dankoshevoy2001@gmail.com>
2025-04-12 01:04:54 +0300

    fix

--- services/ImmoRent/src/services/simulateHuman.js
+++ services/ImmoRent/src/services/simulateHuman.js
@@ -3,29 +3,30 @@ function getRandomInt(min, max) {
 }
 async function simulateHumanInteraction(page) {
 	console.log('🧠 Имитация человека начата...')
-	
+
 	for (let i = 0; i < 7; i++) {
 		const x = getRandomInt(50, 1000)
 		const y = getRandomInt(50, 700)
 		const steps = getRandomInt(5, 10)
-		await page.mouse.move(x, y, {steps})
+		await page.mouse.move(x, y, { steps })
 		await delay(getRandomInt(1500, 2000))
 	}
-	
+
 	console.log('📖 Человек читает страницу...')
 	await delay(getRandomInt(1000, 2000))
-	
+
 	for (let i = 0; i < 4; i++) {
 		const distance = getRandomInt(200, 600)
-		await page.evaluate(d => window.scrollBy(0, d), distance)
+		await page.evaluate((d) => window.scrollBy(0, d), distance)
 		await delay(getRandomInt(500, 1000))
 	}
-	
+
 	await delay(getRandomInt(1000, 2000))
 }
 
 function delay(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms))
+	console.log('💤 Пауза...')
+	return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 module.exports = {
